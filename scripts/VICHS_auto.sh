0a3cd6fbfbfc16aee1a7bfbd2216cf89749dd689
hawkeye116477 <hawkeye116477@gmail.com>
2018-06-22 18:42:29 +0200

    Make PAF modular [ci skip]
    For now at least one

---
+++ scripts/VICHS_auto.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+# Sciezka to miejsce, w którym znajduje się skrypt
+sciezka=$(dirname "$0")
+
+cd $sciezka/..
+
+ost_zmieniony_plik=$(git diff -z --name-only | xargs -0)
+
+katalog=$(dirname $ost_zmieniony_plik)
+
+for i in $ost_zmieniony_plik; do
+    if [ "$i" == "PPB/push.txt" ] || [ "$i" == "PPB_uBlock_AdGuard/push_uBO.txt" ]; then
+        if [[ "$modul" != *" PAF_push.txt"* ]] ;then
+            modul+=" "PAF_push.txt
+        fi
+    fi
+
+    if [[ "$i" == "PPB"* ]]; then
+        if [[ "$glowna_lista" != *" PPB.txt"* ]] ;then
+            glowna_lista+=" "PPB.txt
+        fi
+    fi
+    
+    if [[ "$i" == "PPB_uBlock_AdGuard"* ]]; then
+        glowna_lista+=" "PPB_uBlock_AdGuard.txt
+    fi
+
+done
+
+$sciezka/VICHS.sh $modul $glowna_lista
