5a504bfafe0e52ba87ee6ba9d943989d6a9b9e25
hawkeye116477 <hawkeye116477@gmail.com>
2018-06-23 21:22:10 +0200

    Update script [ci skip]

--- scripts/VICHS_auto.sh
+++ scripts/VICHS_auto.sh
@@ -8,8 +8,6 @@ cd $sciezka/..
 
 ost_zmieniony_plik=$(git diff -z --name-only | xargs -0)
 
-katalog=$(dirname $ost_zmieniony_plik)
-
 for i in $ost_zmieniony_plik; do
     if [ "$i" == "PPB/push.txt" ] || [ "$i" == "PPB_uBlock_AdGuard/push_uBO.txt" ]; then
         if [[ "$modul" != *" PAF_push.txt"* ]] ;then
