f0d1b6e4cf0f095d38754c93663c650a25eb5b5e
hawkeye116477 <hawkeye116477@gmail.com>
2018-06-25 18:42:04 +0200

    Add more templates and update script [ci skip]

--- scripts/VICHS_auto.sh
+++ scripts/VICHS_auto.sh
@@ -14,7 +14,37 @@ for i in $ost_zmieniony_plik; do
             modul+=" "PAF_push.txt
         fi
     fi
-
+    
+    if [ "$i" == "PPB/popupy.txt" ] || [ "$i" == "PPB_uBlock_AdGuard/popupy_uBO.txt" ]; then
+        if [[ "$modul" != *" PAF_popupy.txt"* ]] ;then
+            modul+=" "PAF_popupy.txt
+        fi
+    fi
+    
+    if [ "$i" == "PPB/strzalki.txt" ]; then
+        if [[ "$modul" != *" PAF_strzalki.txt"* ]] ;then
+            modul+=" "PAF_strzalki.txt
+        fi
+    fi
+    
+    if [ "$i" == "PPB/scroll_film.txt" ] || [ "$i" == "PPB_uBlock_AdGuard/scroll_film_uBO.txt" ]; then
+        if [[ "$modul" != *" PAF_scroll_film.txt"* ]] ;then
+            modul+=" "PAF_scroll_film.txt
+        fi
+    fi
+    
+    if [ "$i" == "PPB_uBlock_AdGuard/otagowane_linki_supplement.txt" ]; then
+        if [[ "$modul" != *" PAF_otagowane_linki.txt"* ]] ;then
+            modul+=" "PAF_otagowane_linki.txt
+        fi
+    fi
+    
+    if [ "$i" == "PPB_uBlock_AdGuard/odblokuj_prawy_uBO.txt" ]; then
+        if [[ "$modul" != *" PAF_odblokuj_prawy.txt"* ]] ;then
+            modul+=" "PAF_odblokuj_prawy.txt
+        fi
+    fi
+    
     if [[ "$i" == "PPB"* ]]; then
         if [[ "$glowna_lista" != *" PPB.txt"* ]] ;then
             glowna_lista+=" "PPB.txt
