47c73e1f3e0709f248695797e9c777098853faf7
hawkeye116477 <hawkeye116477@gmail.com>
2018-08-19 11:11:04 +0200

    Fix script [ci skip]

--- scripts/VICHS_auto.sh
+++ scripts/VICHS_auto.sh
@@ -14,67 +14,67 @@ for i in $ost_zmieniony_plik; do
             modul+=" "PAF_push.txt
         fi
     fi
-    
+
     if [ "$i" == "PPB/popupy.txt" ] || [ "$i" == "PPB_uBlock_AdGuard/popupy_uBO.txt" ] || [ "$i" == "PPB_uBlock_AdGuard/popupy_specjalne_instrukcje.txt" ]; then
         if [[ "$modul" != *" PAF_pop-ups.txt"* ]] ;then
             modul+=" "PAF_pop-ups.txt
         fi
     fi
-    
+
     if [ "$i" == "PPB/strzalki.txt" ]; then
         if [[ "$modul" != *" PAF_arrows.txt"* ]] ;then
             modul+=" "PAF_arrows.txt
         fi
     fi
-    
+
     if [ "$i" == "PPB/scroll_film.txt" ] || [ "$i" == "PPB_uBlock_AdGuard/scroll_film_uBO.txt" ]; then
         if [[ "$modul" != *" PAF_scrolling_videos.txt"* ]] ;then
             modul+=" "PAF_scrolling_videos.txt
         fi
     fi
-    
+
     if [ "$i" == "PPB/newslettery_nie_popupy.txt" ] || [ "$i" == "PPB_uBlock_AdGuard/newslettery_uBO.txt" ]; then
         if [[ "$modul" != *" PAF_newsletters.txt"* ]] ;then
             modul+=" "PAF_newsletters.txt
         fi
     fi
-    
+
     if [ "$i" == "PPB_uBlock_AdGuard/otagowane_linki_supplement.txt" ]; then
         if [[ "$modul" != *" PAF_tagged_internal_links.txt"* ]] ;then
             modul+=" "PAF_tagged_internal_links.txt
         fi
     fi
-    
+
     if [ "$i" == "PPB_uBlock_AdGuard/odblokuj_prawy_uBO.txt" ]; then
         if [[ "$modul" != *" PAF_unlocking_right_click.txt"* ]] ;then
             modul+=" "PAF_unlocking_right_click.txt
         fi
     fi
-    
+
     if [ "$i" == "PPB/tla_autoreklamy.txt" ] || [ "$i" == "PPB_uBlock_AdGuard/tla_autoreklamy_uBO.txt" ]; then
         if [[ "$modul" != *" PAF_backgrounds_self-advertising.txt"* ]] ;then
             modul+=" "PAF_backgrounds_self-advertising.txt
         fi
     fi
-    
+
     if [ "$i" == "PPB/widgety_kontaktowe_informacji_zwrotnej.txt" ] || [ "$i" == "PPB_uBlock_AdGuard/widgety_kontaktowe_informacji_zwrotnej_uBO.txt" ]; then
         if [[ "$modul" != *" PAF_contact_feedback_widgets.txt"* ]] ;then
             modul+=" "PAF_contact_feedback_widgets.txt
         fi
     fi
-    
-    if [ "$i" == "PPB/widgety.txt" ] || [ "$i" == "PPB_uBlock_AdGuard/widgety_uBO.txt" || [ "$i" == "PPB_uBlock_AdGuard/widgety_specjalne_instrukcje.txt" ]; then
+
+    if [ "$i" == "PPB/widgety.txt" ] || [ "$i" == "PPB_uBlock_AdGuard/widgety_uBO.txt" ] || [ "$i" == "PPB_uBlock_AdGuard/widgety_specjalne_instrukcje.txt" ]; then
         if [[ "$modul" != *" PAF_other_widgets.txt"* ]] ;then
             modul+=" "PAF_other_widgets.txt
         fi
     fi
-    
+
     if [[ "$i" == "PPB"* ]]; then
         if [[ "$glowna_lista" != *" PPB.txt"* ]] ;then
             glowna_lista+=" "PPB.txt
         fi
     fi
-    
+
     if [[ "$i" == "PPB_uBlock_AdGuard"* ]]; then
         if [[ "$glowna_lista" != *" PPB_uBlock_AdGuard.txt"* ]] ;then
             glowna_lista+=" "PPB_uBlock_AdGuard.txt
