3bdad51d10892db076f7db1bbb1833cb062d9ddd
hawkeye116477 <hawkeye116477@gmail.com>
2019-01-20 13:23:45 +0100

    Update script

--- scripts/VICHS_auto.sh
+++ scripts/VICHS_auto.sh
@@ -8,7 +8,11 @@ cd $sciezka/..
 if [ "$CI" = "true" ]; then
     git config --global user.email "PolishJarvis@int.pl"
     git config --global user.name "PolishJarvis"
-    ost_plik=$(git log --since="6 hours 3 minutes ago" --name-only --pretty=format: | sort | uniq)
+    if [[ "$aktualna_godzina" == "14" ]]; then
+    ost_plik=$(git log --since="6 hours 58 minutes ago" --name-only --pretty=format: | sort | uniq)
+    else
+    ost_plik=$(git log --since="3 hours 58 minutes ago" --name-only --pretty=format: | sort | uniq)
+    fi
 else
     ost_plik=$(git diff -z --name-only | xargs -0)
 fi
