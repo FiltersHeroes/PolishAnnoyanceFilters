2c4036fc11800cf82e565be4c16ac1a01f11ae23
hawkeye116477 <hawkeye116477@gmail.com>
2019-08-02 16:23:11 +0200

    Poprawki

--- scripts/VICHS_auto.sh
+++ scripts/VICHS_auto.sh
@@ -18,61 +18,61 @@ else
 fi
 
 for i in $ost_plik; do
-    if [ "$i" == "PPB/wyjatki.txt" ] || [ "$i" == "PPB/push.txt" ] || [ "$i" == "PPB/uBO_AG/push_suplement.txt" ]; then
+    if [ "$i" == "PPB/push.txt" ] || [ "$i" == "PPB/uBO_AG/push_suplement.txt" ]; then
         if [[ "$modul" != *" PAF_push.txt"* ]]; then
             modul+=" "PAF_push.txt
         fi
     fi
 
-    if [ "$i" == "PPB/wyjatki.txt" ] || [ "$i" == "PPB/uBO_AG/popupy_suplement.txt" ] || [ "$i" == "PPB/uBO_AG/popupy_uBO.txt" ] || [ "$i" == "PPB/uBO_AG/popupy_AG.txt" ] || [ "$i" == "PPB/uBO_AG/popupy_bez_html.txt" ] || [ "$i" == "PPB/uBO_AG/popupy_html.txt" ]; then
+    if [ "$i" == "PPB/uBO_AG/popupy_suplement.txt" ] || [ "$i" == "PPB/uBO_AG/popupy_uBO.txt" ] || [ "$i" == "PPB/uBO_AG/popupy_AG.txt" ] || [ "$i" == "PPB/uBO_AG/popupy_bez_html.txt" ] || [ "$i" == "PPB/uBO_AG/popupy_html.txt" ]; then
         if [[ "$modul" != *" PAF_pop-ups_supp.txt"* ]]; then
             modul+=" "PAF_pop-ups_supp.txt
         fi
     fi
 
-    if [ "$i" == "PPB/wyjatki.txt" ] || [ "$i" == "PPB/popupy.txt" ] || [ "$i" == "PPB/uBO_AG/popupy_suplement.txt" ] || [ "$i" == "PPB/uBO_AG/popupy_uBO.txt" ] || [ "$i" == "PPB/uBO_AG/popupy_AG.txt" ]  || [ "$i" == "PPB/uBO_AG/popupy_bez_html.txt" ] || [ "$i" == "PPB/uBO_AG/popupy_html.txt" ]; then
+    if [ "$i" == "PPB/popupy.txt" ] || [ "$i" == "PPB/uBO_AG/popupy_suplement.txt" ] || [ "$i" == "PPB/uBO_AG/popupy_uBO.txt" ] || [ "$i" == "PPB/uBO_AG/popupy_AG.txt" ]  || [ "$i" == "PPB/uBO_AG/popupy_bez_html.txt" ] || [ "$i" == "PPB/uBO_AG/popupy_html.txt" ]; then
         if [[ "$modul" != *" PAF_pop-ups.txt"* ]]; then
             modul+=" "PAF_pop-ups.txt
         fi
     fi
 
-    if [ "$i" == "PPB/wyjatki.txt" ] || [ "$i" == "PPB/strzalki.txt" ]; then
+    if [ "$i" == "PPB/strzalki.txt" ]; then
         if [[ "$modul" != *" PAF_arrows.txt"* ]] ;then
             modul+=" "PAF_arrows.txt
         fi
     fi
 
-    if [ "$i" == "PPB/wyjatki.txt" ] || [ "$i" == "PPB/scroll_film.txt" ] || [ "$i" == "PPB/uBO_AG/scroll_film_suplement.txt" ] || [ "$i" == "PPB/uBO_AG/scroll_film_userCSS.txt" ] || [ "$i" == "PPB/uBO_AG/scroll_film_non_userCSS.txt" ] || [ "$i" == "PPB/uBO_AG/scroll_film_AG.txt" ]; then
+    if [ "$i" == "PPB/scroll_film.txt" ] || [ "$i" == "PPB/uBO_AG/scroll_film_suplement.txt" ] || [ "$i" == "PPB/uBO_AG/scroll_film_userCSS.txt" ] || [ "$i" == "PPB/uBO_AG/scroll_film_non_userCSS.txt" ] || [ "$i" == "PPB/uBO_AG/scroll_film_AG.txt" ]; then
         if [[ "$modul" != *" PAF_scrolling_videos.txt"* ]]; then
             modul+=" "PAF_scrolling_videos.txt
         fi
     fi
 
-    if [ "$i" == "PPB/wyjatki.txt" ] || [ "$i" == "PPB/newslettery_nie_popupy.txt" ] || [ "$i" == "PPB/uBO_AG/newslettery_suplement.txt" ]; then
+    if [ "$i" == "PPB/newslettery_nie_popupy.txt" ] || [ "$i" == "PPB/uBO_AG/newslettery_suplement.txt" ]; then
         if [[ "$modul" != *" PAF_newsletters.txt"* ]]; then
             modul+=" "PAF_newsletters.txt
         fi
     fi
 
-    if [ "$i" == "PPB/wyjatki.txt" ] || [ "$i" == "PPB/uBO_AG/otagowane_linki_suplement.txt" ]; then
+    if [ "$i" == "PPB/uBO_AG/otagowane_linki_suplement.txt" ]; then
         if [[ "$modul" != *" PAF_tagged_internal_links.txt"* ]]; then
             modul+=" "PAF_tagged_internal_links.txt
         fi
     fi
 
-    if [ "$i" == "PPB/wyjatki.txt" ] || [ "$i" == "PPB/tla_autoreklamy.txt" ] || [ "$i" == "PPB/uBO_AG/tla_autoreklamy_suplement.txt" ]; then
+    if [ "$i" == "PPB/tla_autoreklamy.txt" ] || [ "$i" == "PPB/uBO_AG/tla_autoreklamy_suplement.txt" ]; then
         if [[ "$modul" != *" PAF_backgrounds_self-advertising.txt"* ]]; then
             modul+=" "PAF_backgrounds_self-advertising.txt
         fi
     fi
 
-    if [ "$i" == "PPB/wyjatki.txt" ] || [ "$i" == "PPB/widgety_kontaktowe_informacji_zwrotnej.txt" ]; then
+    if [ "$i" == "PPB/widgety_kontaktowe_informacji_zwrotnej.txt" ]; then
         if [[ "$modul" != *" PAF_contact_feedback_widgets.txt"* ]]; then
             modul+=" "PAF_contact_feedback_widgets.txt
         fi
     fi
 
-    if [ "$i" == "PPB/wyjatki.txt" ] || [ "$i" == "PPB/inne.txt" ] || [ "$i" == "PPB/uBO_AG/inne_AG.txt" ] || [ "$i" == "PPB/uBO_AG/inne_bez_html.txt" ] || [ "$i" == "PPB/uBO_AG/inne_html.txt" ] || [ "$i" == "PPB/uBO_AG/inne_suplement.txt" ]; then
+    if [ "$i" == "PPB/inne.txt" ] || [ "$i" == "PPB/uBO_AG/inne_AG.txt" ] || [ "$i" == "PPB/uBO_AG/inne_bez_html.txt" ] || [ "$i" == "PPB/uBO_AG/inne_html.txt" ] || [ "$i" == "PPB/uBO_AG/inne_suplement.txt" ]; then
         if [[ "$modul" != *" PAF_other_widgets.txt"* ]]; then
             modul+=" "PAF_other_widgets.txt
         fi
